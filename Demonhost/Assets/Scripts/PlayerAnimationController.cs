5ba2dcc7e00b8a515dec8e97066d68b137639046
Jeremiah Sheehy <jameys127@gmail.com>
2025-06-24 00:07:48 -0700

    I added tile sheets and actually have level room I can walk around in with a camera the follows

---
+++ Demonhost/Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private int lastXDirection = -1;
+    private int lastYDirection = -1;
+
+    private int walkLeft = Animator.StringToHash("WalkLeft");
+    private int walkRight = Animator.StringToHash("WalkRight");
+    private int walkBackLeft = Animator.StringToHash("WalkBackLeft");
+    private int walkBackRight = Animator.StringToHash("WalkBackRight");
+    private int idleLeft = Animator.StringToHash("IdleLeft");
+    private int idleRight = Animator.StringToHash("IdleRight");
+    private int idleBackLeft = Animator.StringToHash("IdleBackLeft");
+    private int idleBackRight = Animator.StringToHash("IdleBackRight");
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.Play(idleLeft);
+    }
+
+    public void UpdateAnimation(Vector2 moveInput){
+        if(moveInput.x != 0){
+            lastXDirection = moveInput.x > 0 ? 1 : -1;
+        }
+        if(moveInput.y != 0){
+            lastYDirection = moveInput.y > 0 ? 1 : -1;
+        }
+        PlayAnimation(moveInput);
+    }
+
+    private void PlayAnimation(Vector2 input){
+        if(input.magnitude < 0.1f){
+            if(lastYDirection == -1){
+                Debug.Log(lastXDirection);
+                animator.Play(lastXDirection > 0 ? idleRight : idleLeft);
+                return;
+            }else{
+                animator.Play(lastXDirection > 0 ? idleBackRight : idleBackLeft);
+                return;
+            }
+        }
+        if(input.y > 0.1f){
+            if(input.x > 0.1f){
+                animator.Play(walkBackRight);
+            }else if(input.x < -0.1f){
+                animator.Play(walkBackLeft);
+            }else{
+                animator.Play(lastXDirection > 0 ? walkBackRight : walkBackLeft);
+            }
+        }else if(input.y < -0.1f){
+            if(input.x > 0.1f){
+                animator.Play(walkRight);
+            }else if(input.x < -0.1f){
+                animator.Play(walkLeft);
+            }else{
+                animator.Play(lastXDirection > 0 ? walkRight : walkLeft);
+            }
+        }else{
+            animator.Play(input.x > 0 ? walkRight : walkLeft);
+            lastYDirection = -1;
+        }
+    }
+}
--- Demonhost/Assets/Scripts/PlayerController.cs
+++ Demonhost/Assets/Scripts/PlayerController.cs
@@ -7,39 +7,34 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 5f;
-    private Animator animator;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private PlayerAnimationController playerAnimator;
+    
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        playerAnimator = GetComponent<PlayerAnimationController>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         rb.velocity = moveInput * moveSpeed;
-        // UpdateAnimation();
     }
 
 
     public void Move(InputAction.CallbackContext context){
-        animator.SetBool("isMoving", true);
         if(context.canceled){
-            animator.SetBool("isMoving", false);
-            animator.SetFloat("lastInputX", moveInput.x);
-            animator.SetFloat("lastInputY", moveInput.y);
+            moveInput = Vector2.zero;
+        }else{
+            moveInput = context.ReadValue<Vector2>();
         }
-        moveInput = context.ReadValue<Vector2>();
-        animator.SetFloat("inputX", moveInput.x);
-        animator.SetFloat("inputY", moveInput.y);
-    }
-    private void UpdateAnimation(){
-        bool isMoving = Mathf.Abs(moveInput.y) > 0 || Mathf.Abs(moveInput.x) > 0;
-        animator.SetBool("isMoving", isMoving);
+        if(moveInput.magnitude > 0.1f || context.canceled){
+            playerAnimator.UpdateAnimation(moveInput);
+        }
     }
 }
